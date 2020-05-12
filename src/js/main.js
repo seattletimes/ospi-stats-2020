cc0b106af09b165f93227f92258c157cbf098bcd
llflannery <lflannery@seattletimes.com>
2020-05-12 15:06:46 -0700

    pub

--- src/js/main.js
+++ src/js/main.js
@@ -7,16 +7,24 @@ var $ = require('jquery');
 
 $('.tab').css("visibility","hidden");
 
+// $('.tab').hide();
+
 $( ".header" ).click(function() {
   $('.tab').css("visibility","hidden");
+  // $('.tab').hide();
   $('.header').css("background-color","#efefef");
   $(this).css("background-color","rgb(24, 161, 205)");
-  thisID = $(this).attr('id');
+  var thisID = $(this).attr('id');
   thisID = thisID + "Div";
 
   $('#tabCon').find(`#${thisID}`).css("visibility","inherit");
 
+  var thisHeight = $('#tabCon').find(`#${thisID}`).height();
+  $('figcaption').css('margin-top', (thisHeight + 30));
+  // $('#tabCon').find(`#${thisID}`).show();
 });
 
-$('#tabCon').find(`#childDiv`).css("visibility","inherit");
-$('#child').css("background-color","rgb(24, 161, 205)")
+$('#tabCon').find(`#overviewDiv`).css("visibility","inherit");
+$('#overview').css("background-color","rgb(24, 161, 205)");
+var yay = $('#tabCon').find('#overviewDiv').height();
+$('figcaption').css('margin-top', (yay + 20));
